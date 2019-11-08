95b83552f56eba1250d5d4e42c486cea8ee87660
luisz <lzamora@espol.edu.ec>
2019-11-08 01:31:06 -0500

    primer commit

---
+++ app/src/main/java/com/example/amst3/PerfilUsuario.java
@@ -0,0 +1,92 @@
+package com.example.amst3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerfilUsuario extends AppCompatActivity {
+    TextView txt_id, txt_name, txt_email;
+    ImageView imv_photo;
+    Button btn_logout;
+    DatabaseReference db_reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>) intent.getSerializableExtra("info_user");
+
+        txt_id = findViewById(R.id.txt_userId);
+        txt_name = findViewById(R.id.txt_nombre);
+        txt_email = findViewById(R.id.txt_correo);
+        imv_photo = findViewById(R.id.imv_foto);
+
+        txt_id.setText(info_user.get("user_id"));
+        txt_name.setText(info_user.get("user_name"));
+        txt_email.setText(info_user.get("user_email"));
+        String photo = info_user.get("user_photo");
+        iniciarBaseDeDatos();
+        escribirTweets(info_user.get("user_name"));
+        leerTweets();
+        Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+    }
+
+    public void iniciarBaseDeDatos() {
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Grupo");
+    }
+
+    public void cerrarSesion(View view) {
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+
+    public void leerTweets() {
+
+        db_reference.child("Grupo 7").child("Tweets").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                     System.out.println(snapshot);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println(databaseError.toException());
+            }
+        });
+    }
+     public void escribirTweets(String autor){
+         String tweet = "nueva prueba";
+         String fecha = "31/10/2019";
+         Map<String, String> hola_tweet = new HashMap<String, String>();
+         hola_tweet.put("autor", autor);
+         hola_tweet.put("fecha", fecha);
+         DatabaseReference tweets = db_reference.child("Grupo 7").child("Tweets");
+         tweets.setValue(tweet);
+         tweets.child(tweet).child("autor").setValue(autor);
+         tweets.child(tweet).child("fecha").setValue(fecha);
+    }
+}
+
